f924d5f14382758da1aa5b27aceabd8634900302
wudengguang <wudengguang@isa.qa>
2016-07-03 16:31:26 +0800

    在typings加入了angular-ui-router和angular-material

--- client/typings/index.d.ts
+++ client/typings/index.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="globals/angular-material/index.d.ts" />
+/// <reference path="globals/angular-ui-router/index.d.ts" />
 /// <reference path="globals/angular/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
