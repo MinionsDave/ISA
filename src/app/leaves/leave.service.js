4670fd91b5b14833da6b21f77037f48a117c98d3
MinionsDave <651882883@qq.com>
2016-08-29 23:35:15 +0800

    前端完成了时间的处理

--- src/app/leaves/leave.service.js
+++ src/app/leaves/leave.service.js
@@ -10,18 +10,18 @@ angular.module('nodeInAction')
         */
         switch (leave.startSection) {
             case '中午':
-                leave.startDate = new Date(leave.startDate.getTime() + 12 * 3600);
+                leave.startDate = new Date(leave.startDate.getTime() + 12 * 3600 * 1000);
                 break;
             case '下午':
-                leave.startDate = new Date(leave.startDate.getTime() + 24 * 3600);
+                leave.startDate = new Date(leave.startDate.getTime() + 24 * 3600 * 1000);
                 break;
         }
         switch (leave.endSection) {
             case '中午':
-                leave.endDate = new Date(leave.endDate.getTime() + 12 * 3600)
+                leave.endDate = new Date(leave.endDate.getTime() + 12 * 3600 * 1000);
                 break;
             case '下午':
-                leave.endDate = new Date(leave.endDate.getTime() + 24 * 3600)
+                leave.endDate = new Date(leave.endDate.getTime() + 24 * 3600 * 1000);
                 break;
         }
         return $http.post(Config.URL + '/leave', leave);
