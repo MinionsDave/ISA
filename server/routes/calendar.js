ae5a7aea2250de229e5ab973ca91af1ecf436806
MinionsDave <651882883@qq.com>
2016-09-19 22:41:06 +0800

    加入了moment，完成了查询显示一页日历的接口

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -1,10 +1,24 @@
 const router = require('express').Router();
+const moment = require('moment');
 
 const Calendar = require('../models/calendar');
 
 router.route('/:datetime')
     .get(({ params: { datetime }}, res, next) => {
-        res.json(datetime);
+        let firstDayOfMonth = moment(parseInt(datetime)).startOf('month').startOf('week').valueOf();
+        Calendar.find({
+            date: {$gte: firstDayOfMonth}
+        })
+        .limit(35)
+        .sort({
+            date: 1
+        })
+        .then(dates => {
+            dates.map(x => {
+                x._doc.date = moment(x._doc.date).format('YYYY-MM-DD HH:mm:ss');
+            });
+            res.json(dates);
+        });
     });
 
 module.exports = router;
