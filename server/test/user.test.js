b9ef16508d07d37bd94ad65de85e9767534cadb0
MinionsDave <651882883@qq.com>
2016-09-25 21:10:28 +0800

    写了些测试用例

--- server/test/user.test.js
+++ server/test/user.test.js
@@ -3,10 +3,16 @@ const app = require('../app');
 const request = require('supertest-as-promised');
 
 const expect = chai.expect;
+const User = require('../models/user');
 
 chai.config.includeStack = true;
 
-describe('## User APIs', () => {
+describe('## Account APIs', () => {
+    let user = {
+        username: '6542321@qq.com',
+        password: '5465321'
+    }
+
     describe('# GET /api/account', () => {
         it('should get all users', (done) => {
             request(app)
@@ -14,6 +20,69 @@ describe('## User APIs', () => {
                 .expect(200)
                 .then(res => {
                     expect(res.body).to.be.an('array');
+                    done();
+                })
+                .catch(done);
+        });
+    });
+
+    describe('# POST /api/account', () => {
+        it('should create a new user', (done) => {
+            request(app)
+                .post('/api/account')
+                .send(user)
+                .expect(200)
+                .then(() => User.findOne({
+                    username: user.username
+                }))
+                .then(userObj => {
+                    expect(userObj.username).to.equal(user.username);
+                    user = userObj;
+                    done();
+                })
+                .catch(done);
+        });
+    });
+
+    describe('# GET /api/account/active/:activeToken', () => {
+        it('should active the user', (done) => {
+            request(app)
+                .get(`/api/account/active/${ user.activeToken }`)
+                .expect(200)
+                .then(() => User.findOne({
+                    username: user.username
+                }))
+                .then(userObj => {
+                    expect(userObj.active).to.be.true;
+                    user = userObj;
+                    done();
+                })
+                .catch(done);
+        });
+    });
+
+    describe('# GET /api/account/checkUser/:username', () => {
+        it('should return user is exists', (done) => {
+            request(app)
+                .get(`/api/account/checkUser/${ user.username }`)
+                .expect(400, done);
+        });
+
+        it('should return user is not exists',  (done) => {
+            request(app)
+                .get(`/api/account/checkUser/asdcbrhdhf`)
+                .expect(200, done);
+        });
+
+    });
+
+    describe('# DELETE /api/account/:id', () => {
+        it('should delete the user', (done) => {
+            request(app)
+                .delete(`/api/account/${ user._id }`)
+                .expect(200)
+                .then((res) => {
+                    expect(res.body.message).to.equal('删除成功');
                     done();
                 })
                 .catch(done);
